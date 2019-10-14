db52a03e17864f70e93236e81cf0c61d5deee2aa
Nazir Mohammad <mnazir785@gmail.com>
2019-10-14 13:29:05 +0100

    new commit- Array demo

---
+++ Workspace/src/Array.java
@@ -0,0 +1,68 @@
+public class Array {
+
+
+
+        public static void main (String[] args){
+
+
+
+            //declare an array of integers
+
+            int [] myArray;
+
+            // create an array with new operator
+
+            // allocate memory for 10 integers
+
+            myArray = new int[10];
+
+            // int[] myArray2= new int[2];
+
+            //intialise the first element.
+
+            myArray[0]= 100;
+
+            myArray[1]= 200;
+
+            myArray[2]= 300;
+
+            myArray[3]= 400;
+
+            myArray[4]= 500;
+
+            myArray[5]= 600;
+
+            myArray[6]= 700;
+
+            myArray[7]= 800;
+
+            myArray[8]= 900;
+
+            myArray[9]= 1000;
+
+            System.out.println("Element at index 0 : " + myArray[0]);
+
+            System.out.println("Element at index 0 : " + myArray[1]);
+
+            System.out.println("Element at index 0 : " + myArray[2]);
+
+            System.out.println("Element at index 0 : " + myArray[3]);
+
+            System.out.println("Element at index 0 : " + myArray[4]);
+
+            System.out.println("Element at index 0 : " + myArray[5]);
+
+            System.out.println("Element at index 0 : " + myArray[6]);
+
+            System.out.println("Element at index 0 : " + myArray[7]);
+
+            System.out.println("Element at index 0 : " + myArray[8]);
+
+            System.out.println("Element at index 0 : " + myArray[9]);
+
+
+
+        }
+
+    }
+}
